8495106ec0007671ddf9dcf544c3449fed258552
Siyath Kasali <siyaths19@gmail.com>
2017-10-26 23:44:21 +0530

    second commit

--- app.js
+++ app.js
@@ -2,21 +2,47 @@ var form = document.getElementById('form');
 form.addEventListener('submit', addItem);
 
 var item = document.getElementById('list-item');
+item.addEventListener('click', removeBtn);
 
+var filter = document.getElementById('filter');
+filter.addEventListener('keyup', filterItem);
 
-function addItem(e){
-    e.preventDefault();
-    var input = document.getElementById('input').value;
-    var li = document.createElement('li');
-    li.className = 'list-group-item';
-    li.appendChild(document.createTextNode(input));
+function filterItem(e){
+    console.log(e.target.value);
+}
 
-    var button = document.createElement('button');
-    button.className = 'btn btn-danger float-right delete';
-    button.appendChild(document.createTextNode('x'));
-    console.log(button);
-    item.appendChild(li);
-    item.appendChild(button);
+// Remove button
+function removeBtn(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are you sure')){
+            var li = e.target.parentElement;
+            item.removeChild(li);
+        }
+    }
+}
     
 
+// Add item
+function addItem(e){
+    e.preventDefault();
+    // Get input value
+    var input = document.getElementById('input').value;
+    // Creating a new liste item 
+    var li = document.createElement('li');
+    li.className = 'list-group-item';
+
+    // apeending text inside li
+    li.appendChild(document.createTextNode(input));
+
+    // Creating a button
+    var button = document.createElement('button');
+    button.className = 'btn btn-danger float-right delete';
+
+    // Append text inside button
+    button.appendChild(document.createTextNode('x'));
+    // append button inside button
+    li.appendChild(button);
+
+    // append li in main UL
+    item.appendChild(li);
 }
