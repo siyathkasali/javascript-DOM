bac2f0c415651a38d8dc9b0cc8e04658333b5306
siyath kasali <siyaths19@gmail.com>
2018-01-18 19:14:05 +0530

    number guesser

--- Random Number/app.js
+++ Random Number/app.js
@@ -1,2 +1,48 @@
+/**
+ *  - Player must guess a number between a min and max
+ *  - Player gets a certain amount of guesses
+ *  - Notify player of guessses remaining
+ *  - Notify the player of the correct answer if loose
+ *  - Let player choose to play again 
+ */
+
+let min = 1,
+    max = 10,
+    winingNum = 5;
+    guessesLeft = 3;
+
 const game = document.getElementById('game'),
-    guessInput = document.getElementById('guess-input');
+      minNumber = document.querySelector('.min-value'),
+      maxNumber = document.querySelector('.max-value'),
+      guessBtn = document.getElementById('guess-btn'),
+      guessInput = document.getElementById('guess-input'),
+      message = document.querySelector('.message');
+
+      minNumber.textContent = min;
+      maxNumber.textContent = max;
+
+      guessBtn.addEventListener('click', guessNum);
+
+      function guessNum(){
+            const inVal = parseInt(guessInput.value);
+            
+            if(inVal < min || inVal > max || isNaN(inVal)){
+                notification(`Please enter a valid number between ${min} and ${max}`, 'red');
+            }
+
+            if(inVal === winingNum){
+                guessInput.disabled = true;
+                guessInput.style.border = '1px solid green';
+                notification(`yup won ${winingNum} is correct`, 'green');
+            }else{
+                guessesLeft--;
+                notification(`you have only ${guessesLeft} guess left out of 3`, 'red');
+            }
+      }
+
+      function notification(msg,color){
+           message.textContent = msg;
+           message.style.color = color;
+      }
+
+
